587637f5863490a44ff9a486c80a6bdd59179e46
hsleung95 <hsleung@stu.ust.hk>
2015-11-23 12:42:59 +0800

    Filter test and css for select filter
    Written some test cases for filter and updated css for selecting filter

--- test/unit/filterTest.js
+++ test/unit/filterTest.js
@@ -79,6 +79,85 @@ var questionList=[{
   order: 1
 }];
 
+var timeList=[{
+                  wholeMsg: "newTodo",
+                  head: "head",
+                  headLastChar: "?",
+                  desc: "desc",
+                  linkedDesc: "linkedDesc",
+                  completed: false,
+                  timestamp: 0,
+                  tags: "...",
+                  echo: 3,
+                  order: 1
+                  },{
+                  wholeMsg: "newTodo",
+                  head: "head",
+                  headLastChar: "?",
+                  desc: "desc",
+                  linkedDesc: "linkedDesc",
+                  completed: false,
+                  timestamp: 1,
+                  tags: "...",
+                  echo: 2,
+                  order: 2
+                  },{
+                  wholeMsg: "newTodo",
+                  head: "head",
+                  headLastChar: "?",
+                  desc: "desc",
+                  linkedDesc: "linkedDesc",
+                  completed: false,
+                  timestamp: 1,
+                  tags: "...",
+                  echo: 1,
+                  order: 3
+                  },{
+                  wholeMsg: "newTodo",
+                  head: "head",
+                  headLastChar: "?",
+                  desc: "desc",
+                  linkedDesc: "linkedDesc",
+                  completed: false,
+                  timestamp: 3,
+                  tags: "...",
+                  echo: 2,
+                  order: 4
+                  },{
+                  wholeMsg: "newTodo",
+                  head: "head",
+                  headLastChar: "?",
+                  desc: "desc",
+                  linkedDesc: "linkedDesc",
+                  completed: false,
+                  timestamp: 4,
+                  tags: "...",
+                  echo: 2,
+                  order: 5
+                  },{
+                  wholeMsg: "newTodo",
+                  head: "head",
+                  headLastChar: "?",
+                  desc: "desc",
+                  linkedDesc: "linkedDesc",
+                  completed: false,
+                  timestamp: 5,
+                  tags: "...",
+                  echo: 0,
+                  order: 6
+                  },{
+                  wholeMsg: "newTodo",
+                  head: "head",
+                  headLastChar: "?",
+                  desc: "desc",
+                  linkedDesc: "linkedDesc",
+                  completed: false,
+                  timestamp: 6,
+                  tags: "...",
+                  echo: 0,
+                  order: 7
+                  }];
+
 describe('TodoCtrl', function() {
   beforeEach(module('todomvc'));
 
@@ -93,19 +172,37 @@ describe('TodoCtrl', function() {
     }));
 
     it('Filter order test', inject(function(questionFilterFilter) { // need to put Filter suffix
-      var filteredList = questionFilterFilter(questionList, 100);
+      var filteredList = questionFilterFilter(questionList, 100,0);
       for (var i in filteredList) {
         expect(""+filteredList[i].order).toEqual(i);
       }
     }));
 
     it('Filter max test', inject(function(questionFilterFilter) { // need to put Filter suffix
-      var filteredList = questionFilterFilter(questionList, 1);
+      var filteredList = questionFilterFilter(questionList, 1,0);
       expect(filteredList.length).toEqual(5);
 
       for (var i in filteredList) {
         expect(""+filteredList[i].order).toEqual(i);
       }
     }));
+    
+    it('Filter time test',inject(function(questionFilterFilter){
+      var filteredList = questionFilterFilter(timeList,1,1);
+      expect(filteredList.length).toEqual(5);
+      for(var i in filteredList){
+        expect(""+filteredList[i].order).toEqual(i);
+      }
+    }));
   });
+ describe('momentFilter Testing',function(){
+    beforeEach(module(function($provide) {
+      $provide.value('version', 'TEST_VER'); //TODO: what is this provide?
+      console.log("provide.value: " + $provide.value);
+      }));
+          
+      it('has a moment filter', inject(function($filter) {
+            expect($filter('momentFilter')).not.toBeNull();
+        }));
+    });
 });
